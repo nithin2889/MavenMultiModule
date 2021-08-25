bde69603a061f5b65e3ab2fd17807863422d6e5e
Nithin <nithin2889@gmail.com>
2021-08-26 00:56:52 +0530

    create multi-module project with dependencies

---
+++ jpa-entities/src/main/java/entities/User.java
@@ -0,0 +1,26 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String firstName;
+  private String lastName;
+  private String email;
+}
